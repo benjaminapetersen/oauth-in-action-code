d1b87fe28717174f7f2cc9e8a12c9d21c8df9557
Benjamin A. Petersen <ben@benjaminapetersen.me>
2023-05-12 16:45:38 -0400

    hacks

--- exercises/ch-3-ex-1/authorizationServer.js
+++ exercises/ch-3-ex-1/authorizationServer.js
@@ -27,7 +27,9 @@ var authServer = {
 // client information
 var clients = [
 	{
+		// only one client, with a pretty generic name
 		"client_id": "oauth-client-1",
+		// and a really terrible secret
 		"client_secret": "oauth-client-secret-1",
 		"redirect_uris": ["http://localhost:9000/callback"],
 		"scope": "foo bar"
@@ -35,31 +37,58 @@ var clients = [
 ];
 
 var codes = {};
-
+// its important to track the requests, though I do not know that we are
+// making use of a great security model here.
+// pairing up the specific request with the client that made the request
+// is a critical security component, I believe part of the PKCE extension.
+// However, I believe PKCE would use 3 parts:
+// - code_challenge_method likely sha256
+// - code_challenge the actual sha256 value of the code
+// - code_verifier which is the original code
+// How it works:
+// - the client sends the method & challenge(sha256 hash) to the server
+//   as proof of who it is
+// - the server would send back an auth code
+// - the client sends the auth code and the verifier(original random string)
+//   to say "hey, i have the code, and I have the pre-hashed string to prove"
+//   I'm still who you were talking to.
+//   The server can re-hash the string, see that it matches the challenge, and
+//   then confidently give the token back to the client
 var requests = {};
 
 var getClient = function(clientId) {
-	return __.find(clients, function(client) { return client.client_id == clientId; });
+	let foundClient = __.find(clients, function(client) { return client.client_id == clientId; });
+	console.log('getClient()', foundClient);
+	return foundClient;
 };
 
 app.get('/', function(req, res) {
+	console.log('GET', '/index', 'clients', clients, 'authServer', authServer)
 	res.render('index', {clients: clients, authServer: authServer});
 });
 
+let authorizeCounter = 0;
 app.get("/authorize", function(req, res){
-	
+
+	console.log('GET', '/authorize', 'authorize counter', authorizeCounter); authorizeCounter++
+
+	// client_id must match one of the expected clients, or we error
 	var client = getClient(req.query.client_id);
-	
+
+	// is the client ok?
 	if (!client) {
 		console.log('Unknown client %s', req.query.client_id);
 		res.render('error', {error: 'Unknown client'});
 		return;
+	// even if we got a good client_id, we are going to make sure other things are as expected
+	// the redirect_uri MUST be an expected value else fail.
+	// we don't want to send anything back to an incorrect client
 	} else if (!__.contains(client.redirect_uris, req.query.redirect_uri)) {
 		console.log('Mismatched redirect URI, expected %s got %s', client.redirect_uris, req.query.redirect_uri);
 		res.render('error', {error: 'Invalid redirect URI'});
 		return;
 	} else {
-		
+
 		var rscope = req.query.scope ? req.query.scope.split(' ') : undefined;
 		var cscope = client.scope ? client.scope.split(' ') : undefined;
 		if (__.difference(rscope, cscope).length > 0) {
@@ -71,41 +100,65 @@ app.get("/authorize", function(req, res){
 			res.redirect(url.format(urlParsed));
 			return;
 		}
-		
+
 		var reqid = randomstring.generate(8);
-		
+		console.log('GET', '/authorize', 'reqid', reqid);
+
 		requests[reqid] = req.query;
-		
+		console.log('GET', '/authorize', 'requests', 'new request saved', requests, 'from query', req.query, reqid)
+		console.log('GET', '/authorize', 'requests:', requests[reqid] = req.query);
 		res.render('approve', {client: client, reqid: reqid, scope: rscope});
 		return;
 	}
 
 });
 
+let approveCounter = 0
+
 app.post('/approve', function(req, res) {
 
+	// too much in these objects
+	// console.log('POST', '/approve', req)
+	// console.log('POST', '/approve', res)
+	console.log('POST', '/approve', 'approve counter', approveCounter); approveCounter++
 	var reqid = req.body.reqid;
 	var query = requests[reqid];
+	console.log('POST', '/approve', 'requid', reqid)
+	console.log('POST', '/approve', 'query', query)
+	console.log('POST', '/approve', 'requests', 'existing requests', requests)
+	console.log('POST', '/approve', 'requests', 'via reqid (query)', query, requests[reqid])
 	delete requests[reqid];
 
 	if (!query) {
 		// there was no matching saved request, this is an error
+		console.log('POST', '/approve', 'query', 'is empty')
 		res.render('error', {error: 'No matching authorization request'});
 		return;
 	}
-	
+
 	if (req.body.approve) {
+		console.log('POST', '/approve', 'approve?', req.body.approve)
 		if (query.response_type == 'code') {
+			console.log('POST', '/approve', 'code?', query.response_type == 'code')
 			// user approved access
 			var code = randomstring.generate(8);
-			
+			console.log('POST', '/approve', 'code', code)
+
 			var user = req.body.user;
-		
+			console.log('POST', '/approve', 'user', user)
+
 			var scope = __.filter(__.keys(req.body), function(s) { return __.string.startsWith(s, 'scope_'); })
 				.map(function(s) { return s.slice('scope_'.length); });
+			console.log('POST', '/approve', 'scope', scope)
+
 			var client = getClient(query.client_id);
+			console.log('POST', '/approve', 'client', client)
+
 			var cscope = client.scope ? client.scope.split(' ') : undefined;
+			console.log('POST', '/approve', 'cscope', cscope)
+
 			if (__.difference(scope, cscope).length > 0) {
+				console.log('POST', '/approve', 'different scope', scope, cscope)
 				// client asked for a scope it couldn't have
 				var urlParsed = url.parse(query.redirect_uri);
 				delete urlParsed.search; // this is a weird behavior of the URL library
@@ -117,37 +170,57 @@ app.post('/approve', function(req, res) {
 
 			// save the code and request for later
 			codes[code] = { authorizationEndpointRequest: query, scope: scope, user: user };
-		
+			console.log('POST', '/approve', 'codes', codes)
+
 			var urlParsed =url.parse(query.redirect_uri);
 			delete urlParsed.search; // this is a weird behavior of the URL library
 			urlParsed.query = urlParsed.query || {};
 			urlParsed.query.code = code;
-			urlParsed.query.state = query.state; 
+			urlParsed.query.state = query.state;
+
+			console.log('POST', '/approve', 'redirect', urlParsed)
+			// here we send the redirect back to the browser, which will follow it
+			// back to the client.  its a little odd, but important to note how this
+			// works. the redirect does not go directly to the client, but rather,
+			// the browser which will follow it to the client.
 			res.redirect(url.format(urlParsed));
+			console.log('POST', '/approve', 'after redirect (return)')
 			return;
 		} else {
+			console.log('POST', '/approve', 'unsupported_response_type')
 			// we got a response type we don't understand
 			var urlParsed =url.parse(query.redirect_uri);
 			delete urlParsed.search; // this is a weird behavior of the URL library
 			urlParsed.query = urlParsed.query || {};
 			urlParsed.query.error = 'unsupported_response_type';
+			// here we send the redirect back to the browser, which will follow it
+			// back to the client.  its a little odd, but important to note how this
+			// works. the redirect does not go directly to the client, but rather,
+			// the browser which will follow it to the client.
+			console.log('POST', '/approve', 'redirect', urlParsed)
 			res.redirect(url.format(urlParsed));
 			return;
 		}
 	} else {
 		// user denied access
+		console.log('POST', '/approve', 'user denied')
 		var urlParsed =url.parse(query.redirect_uri);
 		delete urlParsed.search; // this is a weird behavior of the URL library
 		urlParsed.query = urlParsed.query || {};
 		urlParsed.query.error = 'access_denied';
+		console.log('POST', '/approve', 'redirect', urlParsed)
+		// here we send the redirect back to the browser, which will follow it
+		// back to the client.  its a little odd, but important to note how this
+		// works. the redirect does not go directly to the client, but rather,
+		// the browser which will follow it to the client.
 		res.redirect(url.format(urlParsed));
 		return;
 	}
-	
+
 });
 
 app.post("/token", function(req, res){
-	
+
 	var auth = req.headers['authorization'];
 	if (auth) {
 		// check the auth header
@@ -155,7 +228,7 @@ app.post("/token", function(req, res){
 		var clientId = querystring.unescape(clientCredentials[0]);
 		var clientSecret = querystring.unescape(clientCredentials[1]);
 	}
-	
+
 	// otherwise, check the post body
 	if (req.body.client_id) {
 		if (clientId) {
@@ -164,28 +237,28 @@ app.post("/token", function(req, res){
 			res.status(401).json({error: 'invalid_client'});
 			return;
 		}
-		
+
 		var clientId = req.body.client_id;
 		var clientSecret = req.body.client_secret;
 	}
-	
+
 	var client = getClient(clientId);
 	if (!client) {
 		console.log('Unknown client %s', clientId);
 		res.status(401).json({error: 'invalid_client'});
 		return;
 	}
-	
+
 	if (client.client_secret != clientSecret) {
 		console.log('Mismatched client secret, expected %s got %s', client.client_secret, clientSecret);
 		res.status(401).json({error: 'invalid_client'});
 		return;
 	}
-	
+
 	if (req.body.grant_type == 'authorization_code') {
-		
+
 		var code = codes[req.body.code];
-		
+
 		if (code) {
 			delete codes[req.body.code]; // burn our code, it's been used
 			if (code.authorizationEndpointRequest.client_id == clientId) {
@@ -206,7 +279,7 @@ app.post("/token", function(req, res){
 
 				res.status(200).json(token_response);
 				console.log('Issued tokens for code %s', req.body.code);
-				
+
 				return;
 			} else {
 				console.log('Client mismatch, expected %s got %s', code.authorizationEndpointRequest.client_id, clientId);
@@ -235,4 +308,3 @@ var server = app.listen(9001, 'localhost', function () {
 
   console.log('OAuth Authorization Server is listening at http://%s:%s', host, port);
 });
- 
--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -15,6 +15,8 @@ app.set('view engine', 'html');
 app.set('views', 'files/client');
 
 // authorization server information
+// the client only needs to know about these two endpoints.
+// the client doesn't need to know anything else about the server at all.
 var authServer = {
 	authorizationEndpoint: 'http://localhost:9001/authorize',
 	tokenEndpoint: 'http://localhost:9001/token'
@@ -27,11 +29,15 @@ var authServer = {
  * Add the client information in here
  */
 var client = {
-	"client_id": "",
-	"client_secret": "",
+	// TODO: need values here to match that which is configured in authorizationServer.js
+	"client_id": "oauth-client-1",
+	// this approach cannot be used with a front end JavaScript client.
+	// the secret would be in the brower and therefore provide no security benefit.
+	"client_secret": "oauth-client-secret-1",
 	"redirect_uris": ["http://localhost:9000/callback"]
 };
 
+// all we need to know at this point is where the protected resource is
 var protectedResource = 'http://localhost:9002/resource';
 
 var state = null;
@@ -48,15 +54,68 @@ app.get('/authorize', function(req, res){
 	/*
 	 * Send the user to the authorization server
 	 */
-	
+	let authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
+		response_type: 'code',
+		client_id: client.client_id,
+		redirect_uri: client.redirect_uris[0]
+	})
+	console.log('GET','/authorize', 'REDIRECT', authorizeUrl);
+	res.redirect(authorizeUrl)
 });
 
+// /callback is for the redirect_uri that is passed to the authorization server.
+// the authorization server, if the flow is successful, will send a redirect
+// to the browser. the browser will follow the redirect back to the client,
+// which will pick up here.
 app.get('/callback', function(req, res){
 
 	/*
 	 * Parse the response from the authorization server and get a token
 	 */
-	
+	console.log('GET', '/callback')
+	let code = req.query.code
+	console.log('GET', '/callback', 'code', code)
+
+	// and we will simply POST the authorization code to the /token endpoint
+	// of the authorization server in order to get back a token.
+	var form_data = qs.stringify({
+		grant_type: 'authorization_code',
+		code: code,
+		// note that this is not going to be a redirect again, so this
+		// value is not used.  however, the OAuth code specification requires
+		// that the redirect_uri is sent again to the token request.
+		// this is intended as a security measure to prevent a compromised redirect_uri
+		// with a well-meaning client by injecting an auth code from one session to another.
+		redirect_uri: client.redirect_uris[0]
+	})
+	console.log('GET', '/callback', 'form_data', form_data);
+
+	var headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Basic ' + encodeClientCredentials(client.client_id, client.client_secret)
+	}
+	console.log('GET', '/callback', 'headers', headers)
+
+	// this might immediately create a POST? do we send it? not familar with this lib...
+	let tokRes = request('POST', authServer.tokenEndpoint, {
+		body: form_data,
+		headers: headers
+	})
+
+	console.log('requesting access token for code %s', code)
+
+	if (tokRes.statusCode >= 200 && tokRes.statusCode < 300) {
+		let body = JSON.parse(tokRes.getBody())
+		console.log('GET', '/callback', 'token POST request response body', body)
+		// store it as a global var
+		access_token = body.access_token;
+		console.log('GET', '/callback', 'got access token', access_token);
+		res.render('index', {access_token: access_token, scope: scope})
+	} else {
+		res.render('error', {error: 'unable to fetch access token, server response: ' + tokenRes.statusCode})
+		console.log('GET', '/callback', 'error')
+	}
+
 });
 
 app.get('/fetch_resource', function(req, res) {
@@ -64,7 +123,7 @@ app.get('/fetch_resource', function(req, res) {
 	/*
 	 * Use the access token to call the resource server
 	 */
-	
+
 });
 
 var buildUrl = function(base, options, hash) {
@@ -79,19 +138,25 @@ var buildUrl = function(base, options, hash) {
 	if (hash) {
 		newUrl.hash = hash;
 	}
-	
-	return url.format(newUrl);
+	let formatted = url.format(newUrl);
+	console.log("buildUrl", base, options, hash);
+	console.log("formatted:", formatted);
+	return formatted
 };
 
 var encodeClientCredentials = function(clientId, clientSecret) {
-	return Buffer.from(querystring.escape(clientId) + ':' + querystring.escape(clientSecret)).toString('base64');
+	console.log("encodeClientCredentials()", clientId, clientSecret);
+	let result = Buffer.from(querystring.escape(clientId) + ':' + querystring.escape(clientSecret)).toString('base64');
+	console.log("result", result);
+	return result
 };
 
+// the root route, this seems to match the top app.get("/") index route
 app.use('/', express.static('files/client'));
 
+// setup the server
 var server = app.listen(9000, 'localhost', function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('OAuth Client is listening at http://%s:%s', host, port);
 });
- 
